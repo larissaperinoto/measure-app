dc78b69e45c6663a7d33450faa586d2229d71252
Larissa Perinoto <perinotolarissa@gmail.com>
2024-08-29 10:48:14 -0300

    feat: cria middlewares de validação de body request

---
+++ src/middlewares/measure.confirm.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { validateOrReject } from "class-validator";
+import { CreateMeasureValidatorSchema } from "../utils/validators/measure.validator";
+import { httpStatus } from "../utils/httpStatus";
+
+export async function confirmMeasureValidator(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const measure = new CreateMeasureValidatorSchema();
+    measure.image = req.body.image;
+    measure.customer_code = req.body.customer_code;
+    measure.measure_datetime = req.body.measure_datetime;
+    measure.measure_type = req.body.measure_type;
+
+    await validateOrReject(measure);
+
+    next();
+  } catch (e: any) {
+    console.error(e);
+    const description = Object.values(e[0].constraints)[0];
+    res.status(httpStatus.BadRequest).send({
+      error_code: "INVALID_DATA",
+      error_description: description,
+    });
+  }
+}
---
+++ src/middlewares/measure.create.middleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { validateOrReject } from "class-validator";
+import { ConfirmMeasureValidatorSchema } from "../utils/validators/measure.validator";
+import { httpStatus } from "../utils/httpStatus";
+
+export async function createMeasureValidator(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const measure = new ConfirmMeasureValidatorSchema();
+    measure.measure_uuid = req.body.measure_uuid;
+    measure.confirmed_value = req.body.confirmed_value;
+
+    await validateOrReject(measure);
+
+    next();
+  } catch (e: any) {
+    console.error(e);
+    const description = Object.values(e[0].constraints)[0];
+    res.status(httpStatus.BadRequest).send({
+      error_code: "INVALID_DATA",
+      error_description: description,
+    });
+  }
+}
