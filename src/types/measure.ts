d09b7ed936a4292ecb732a12f8bb5c013c71cd61
Larissa Perinoto <perinotolarissa@gmail.com>
2024-08-29 10:49:32 -0300

    feat: cria tipos para o body da requisição

---
+++ src/types/measure.ts
@@ -0,0 +1,16 @@
+export enum MeasureTypes {
+  WATER,
+  GAS,
+}
+
+export type Measure = {
+  image: string;
+  customer_code: string;
+  measure_datetime: Date;
+  measure_type: MeasureTypes;
+};
+
+export type MeasureConfirm = {
+  measure_uuid: "string";
+  confirmed_value: number;
+};
